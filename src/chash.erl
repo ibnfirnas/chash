b0ef072ddc2da38021f97c0b0f2f04eee3db46cd
Siraaj Khandkar <siraaj@khandkar.net>
2014-04-18 01:11:07 -0400

    Cleanup whitespace.

--- src/chash.erl
+++ src/chash.erl
@@ -51,7 +51,7 @@
          update/3]).
 
 -export_type([chash/0, index/0, index_as_int/0]).
-    
+
 -define(RINGTOP, trunc(math:pow(2,160)-1)).  % SHA-1 space
 
 -ifdef(TEST).
@@ -109,7 +109,7 @@ sha(Bin) ->
 %%      into the ring.  Two names with the same SHA-1 hash value are
 %%      considered the same name.
 -spec key_of(ObjectName :: term()) -> index().
-key_of(ObjectName) ->    
+key_of(ObjectName) ->
     sha(term_to_binary(ObjectName)).
 
 %% @doc Return all Nodes that own any partitions in the ring.
@@ -125,7 +125,7 @@ members(CHash) ->
 merge_rings(CHashA,CHashB) ->
     {NumPartitions, NodesA} = CHashA,
     {NumPartitions, NodesB} = CHashB,
-    {NumPartitions, [{I,random_node(A,B)} || 
+    {NumPartitions, [{I,random_node(A,B)} ||
            {{I,A},{I,B}} <- lists:zip(NodesA,NodesB)]}.
 
 %% @doc Given the integer representation of a chash key,
@@ -193,10 +193,10 @@ successors(Index, CHash, N) ->
     Ordered = ordered_from(Index, CHash),
     {NumPartitions, _Nodes} = CHash,
     if Num =:= NumPartitions ->
-	    Ordered;
-       true ->
-	    {Res, _} = lists:split(Num, Ordered),
-	    Res
+        Ordered;
+    true ->
+        {Res, _} = lists:split(Num, Ordered),
+        Res
     end.
 
 %% @doc Make the partition beginning at IndexAsInt owned by Name'd node.
@@ -230,11 +230,11 @@ random_node(NodeA,NodeB) -> lists:nth(random:uniform(2),[NodeA,NodeB]).
 
 update_test() ->
     Node = 'old@host', NewNode = 'new@host',
-    
+
     % Create a fresh ring...
     CHash = chash:fresh(5, Node),
     GetNthIndex = fun(N, {_, Nodes}) -> {Index, _} = lists:nth(N, Nodes), Index end,
-    
+
     % Test update...
     FirstIndex = GetNthIndex(1, CHash),
     ThirdIndex = GetNthIndex(3, CHash),
@@ -250,7 +250,7 @@ max_n_test() ->
     CHash = chash:fresh(8, the_node),
     ?assertEqual(1, max_n(1,CHash)),
     ?assertEqual(8, max_n(11,CHash)).
-    
+
 simple_size_test() ->
     ?assertEqual(8, length(chash:nodes(chash:fresh(8,the_node)))).
 
